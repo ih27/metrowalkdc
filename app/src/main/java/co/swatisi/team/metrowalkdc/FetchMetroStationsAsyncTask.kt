f38e987172e2def03d6883a30eac29e6db5ffaa6
Ismayil Hasanov <ismailhasanov@gmail.com>
2017-09-19 23:54:18 -0400

    Added initial WMATA API call code

--- app/src/main/java/co/swatisi/team/metrowalkdc/FetchMetroStationsAsyncTask.kt
+++ app/src/main/java/co/swatisi/team/metrowalkdc/FetchMetroStationsAsyncTask.kt
@@ -1,5 +1,28 @@
 package co.swatisi.team.metrowalkdc
 
+import android.content.Context
+import android.util.Log
+import com.google.gson.JsonObject
+import com.koushikdutta.ion.Ion
+
 /**
  * Created by ihasanov on 9/19/17.
  */
+object FetchMetroStationsAsyncTask {
+
+    private const val METRO_API = "https://api.wmata.com/Rail.svc/json/jStations"
+    private const val METRO_API_TOKEN = "372279dd0fae484ea9c426f476031703"
+    private const val TAG = "FetchMetroStations"
+
+    fun getStationList(context: Context): JsonObject? {
+        try {
+            return Ion.with(context).load(METRO_API)
+                    .addHeader("api-key", METRO_API_TOKEN)
+                    .asJsonObject().get()
+        } catch (e: Exception) {
+            Log.e(TAG, e.message)
+            return null
+        }
+    }
+}
+

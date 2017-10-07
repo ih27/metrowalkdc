ede7b7e330a8ab24d80d9cccb8b7b69602eab746
Ismayil Hasanov <ismailhasanov@gmail.com>
2017-10-07 17:43:24 -0400

    Share functionality is implemented with ShareActionProvider class

--- app/src/main/java/co/swatisi/team/metrowalkdc/activity/LandmarkDetailActivity.kt
+++ app/src/main/java/co/swatisi/team/metrowalkdc/activity/LandmarkDetailActivity.kt
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.support.v7.widget.ShareActionProvider
 import co.swatisi.team.metrowalkdc.R
 import co.swatisi.team.metrowalkdc.model.Landmark
 import co.swatisi.team.metrowalkdc.utility.PersistenceManager
 import kotlinx.android.synthetic.main.activity_landmark_detail.*
+import android.content.Intent
+import android.support.v4.view.MenuItemCompat
+
 
 class LandmarkDetailActivity : AppCompatActivity() {
     private val tag = "LandmarkDetailActivity"
@@ -15,6 +19,7 @@ class LandmarkDetailActivity : AppCompatActivity() {
     private var isFavorite = false
     private lateinit var landmark: Landmark
     private lateinit var persistenceManager: PersistenceManager
+    private var shareActionProvider: ShareActionProvider? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,18 +40,25 @@ class LandmarkDetailActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present
-        menuInflater.inflate(R.menu.favorite_menu, menu)
+        menuInflater.inflate(R.menu.landmark_detail_menu, menu)
         this.menu = menu
 
         // If the landmark is saved to the favorites, change the icon
         if(persistenceManager.isFavorite(landmark))
             favoriteButtonOn()
+
+        // Locate MenuItem with ShareActionProvider
+        val shareButton = menu.findItem(R.id.share_menu_item)
+        // Fetch and store ShareActionProvider
+        shareActionProvider = MenuItemCompat.getActionProvider(shareButton) as ShareActionProvider
+        setShareIntent()
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.favorite -> {
+            R.id.favorite_menu_item -> {
                 // Favorite button clicked
                 toggle()
                 true
@@ -76,14 +88,24 @@ class LandmarkDetailActivity : AppCompatActivity() {
     }
 
     private fun favoriteButtonOn() {
-        val item = menu.findItem(R.id.favorite)
+        val item = menu.findItem(R.id.favorite_menu_item)
         item.setIcon(R.drawable.ic_favorite_on)
         isFavorite = true
     }
 
     private fun favoriteButtonOff() {
-        val item = menu.findItem(R.id.favorite)
+        val item = menu.findItem(R.id.favorite_menu_item)
         item.setIcon(R.drawable.ic_favorite_off)
         isFavorite = false
+    }
+
+    // Call to set the share intent
+    private fun setShareIntent() {
+        // Share button implicit intent
+        val shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        shareIntent.type = getString(R.string.share_mime_type)
+        shareIntent.putExtra(Intent.EXTRA_TEXT, landmark.textToShare())
+        shareActionProvider?.setShareIntent(shareIntent)
     }
 }
--- app/src/main/java/co/swatisi/team/metrowalkdc/model/Landmark.kt
+++ app/src/main/java/co/swatisi/team/metrowalkdc/model/Landmark.kt
@@ -7,4 +7,13 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Landmark(val id: String, val name: String, val imageURL: String, val displayAddress: String,
                     val lat: Double, val lon: Double, val rating: Double, val reviewCount: Int,
-                    val distance: Double = 0.0): Parcelable
+                    val distance: Double = 0.0): Parcelable {
+
+    // Text to show when sharing
+    fun textToShare(): String {
+        return "Name: $name \n" +
+                "Address: $displayAddress \n" +
+                "Rating: $rating \n" +
+                "Reviews: $reviewCount"
+    }
+}
